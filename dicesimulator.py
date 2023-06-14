d175516ea6858e3fe0414aa442a925ca4cf71be3
Dhwani <dhwanibudhiraja2004@gmail.com>
2023-06-14 13:46:32 +0530

    Added a new feature for more number of dice

--- dicesimulator.py
+++ dicesimulator.py
@@ -1,66 +1,64 @@
 
 print("""---------------DICE SIMULATOR------------------""")
 import random 
-
-choice=input("Do you want to roll the dice? Y/N \n")
-if choice.upper() == "Y":
-    choice = True
-else:
-    choice = False
-    print("Simulation finished.")
-
-
-    
+choice = True
+die_no= int(input("Enter how many dice you want to roll: "))
+list_die=[]
+for i in range(0, die_no):
+    rand_no= random.randint(1,6)
+    list_die.append(rand_no)
+     
 while choice:
-    roll=random.randrange(1,7)
-    if choice == True:
-        print(f"You rolled a {roll}")
-    else:
-        
-        break
-    if roll == 1:
-        print(""" 
-            |------------|
-            |            |
-            |     0      |
-            |            |
-            |------------| """)
-    elif roll == 2:
-        print("""
-            |------------|
-            |            |
-            |   0    0   |
-            |            |
-            |------------| """)
-    elif roll == 3:
-        print("""
-            |------------|
-            |     0      |
-            |            |
-            |  0     0   |
-            |------------| """)
-    elif roll == 4:
-        print("""
-            |------------|
-            |   0    0   |
-            |            |
-            |   0    0   |
-            |------------| """)
-    elif roll == 5:
-        print("""
-            |------------|
-            |  0     0   |
-            |     0      |
-            |  0     0   |
-            |------------|""") 
-    elif roll == 6:
-        print("""
-            |------------|
-            |  0   0   0 |
-            |            |
-            |  0   0   0 |
-            |------------| """)
-   
+    
+    for roll in list_die:
+        if choice == True:
+            print(f"You rolled a {roll}")
+        else:
+            
+            break
+        if roll == 1:
+            print(""" 
+                |------------|
+                |            |
+                |     0      |
+                |            |
+                |------------| """)
+        elif roll == 2:
+            print("""
+                |------------|
+                |            |
+                |   0    0   |
+                |            |
+                |------------| """)
+        elif roll == 3:
+            print("""
+                |------------|
+                |     0      |
+                |            |
+                |  0     0   |
+                |------------| """)
+        elif roll == 4:
+            print("""
+                |------------|
+                |   0    0   |
+                |            |
+                |   0    0   |
+                |------------| """)
+        elif roll == 5:
+            print("""
+                |------------|
+                |  0     0   |
+                |     0      |
+                |  0     0   |
+                |------------|""") 
+        elif roll == 6:
+            print("""
+                |------------|
+                |  0   0   0 |
+                |            |
+                |  0   0   0 |
+                |------------| """)
+    
 
     ch = input("Do you want to roll again? (y/n): ")
     if ch.upper() == "Y":
@@ -68,4 +66,4 @@ while choice:
     else:
         choice = False
         print("Simulation finished.")
-          
+        
